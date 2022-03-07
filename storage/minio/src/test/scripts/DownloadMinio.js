1a47a72c15ed6314f58aaa89105ba4b5caff3dd0
Austeja Kalpakovaite <austeja.kalpakovaite@bentley.com>
2022-03-07 10:30:32 +0200

    Marking file as excutable for linux download

--- storage/minio/src/test/scripts/DownloadMinio.js
+++ storage/minio/src/test/scripts/DownloadMinio.js
@@ -5,15 +5,28 @@ const fs = require("fs");
 const https = require("https");
 const constants = require("./MinioConstants");
 
-function downloadMinioExecutable() {
+function downloadFile(targetFilePath, executableDownloadLink) {
+  return new Promise((resolve) => {
+    const destinationFile = fs.createWriteStream(targetFilePath);
+    destinationFile.on('finish', () => {
+      resolve();
+    });
+
+    https.get(executableDownloadLink, (response) => {
+      response.pipe(destinationFile);
+    });
+  });
+}
+
+async function downloadMinioExecutable() {
   const { executableDownloadLink, targetFileDirectory, targetFilePath } = constants.resolveFileProperties();
   if (!fs.existsSync(targetFileDirectory))
     fs.mkdirSync(targetFileDirectory, { recursive: true });
 
-  const destinationFile = fs.createWriteStream(targetFilePath);
-  https.get(executableDownloadLink, (response) => {
-    response.pipe(destinationFile);
-  });
+  await downloadFile(targetFilePath, executableDownloadLink);
+  if (process.platform === "linux") {
+    fs.chmodSync(targetFilePath, 0o755);
+  }
 }
 
 downloadMinioExecutable();
