850fb53029e959060c5ca456e7d93ac86eda7926
Austėja Kalpakovaitė <70565417+austeja-bentley@users.noreply.github.com>
2022-04-01 10:03:34 +0300

    Recreating directories after object deletion in `MinioServerStorage` (#18)
    * Added a step to recreate directory after object deletion in MinioServerStorage
    
    * Moved the variable to test scope
    
    * Fixed test to not create the same directory twice

---
+++ storage/minio/src/MinioServerStorage.ts
@@ -0,0 +1,29 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import {
+  ObjectReference,
+  PresignedUrlProvider,
+  TransferConfigProvider,
+} from "@itwin/object-storage-core";
+import { S3ClientWrapper, S3ServerStorage } from "@itwin/object-storage-s3";
+
+export class MinioServerStorage extends S3ServerStorage {
+  public constructor(
+    s3Client: S3ClientWrapper,
+    presignedUrlProvider: PresignedUrlProvider,
+    transferConfigProvider: TransferConfigProvider
+  ) {
+    super(s3Client, presignedUrlProvider, transferConfigProvider);
+  }
+
+  public override async deleteObject(
+    reference: ObjectReference
+  ): Promise<void> {
+    await super.deleteObject(reference);
+
+    if (!(await this.baseDirectoryExists(reference)))
+      await this.create(reference);
+  }
+}
