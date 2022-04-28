3c48d75f3c0a714f321035459554d61369422295
Austeja Kalpakovaite <austeja.kalpakovaite@bentley.com>
2022-04-28 17:31:17 +0300

    WIP

--- tests/frontend-storage/cypress/integration/FrontendStorage.spec.ts
+++ tests/frontend-storage/cypress/integration/FrontendStorage.spec.ts
@@ -22,14 +22,14 @@ describe(`${FrontendStorage.name}: ${frontendStorage.constructor.name}`, () => {
   });
 
   it("should download file from url", async () => {
-    const testFilePayload = "test payload";
-    const downloadUrl: string = await backendClient.getTestDownloadUrl(testFilePayload);
-    expect(downloadUrl).to.not.be.empty;
+    // const testFilePayload = "test payload";
+    // const downloadUrl: string = await backendClient.getTestDownloadUrl(testFilePayload);
+    // expect(downloadUrl).to.not.be.empty;
 
     // TODO: add correct assertions
-    // const downloadedFileContents: Buffer = (await frontendStorage.download({
-    //   transferType: "buffer",
-    //   url: downloadUrl
-    // }));
+    await frontendStorage.download({
+      transferType: "buffer",
+      url: "https://austejaobjectstorage.blob.core.windows.net/base-dir-49e2d38b-1dc9-44c9-bd72-b3396c93519a/file-c3855889-3487-4d3e-bb64-3f8ee3c90b5d?sp=r&st=2022-04-28T12:41:18Z&se=2022-04-28T20:41:18Z&spr=https&sv=2020-08-04&sr=b&sig=tHITJ3N%2BTbrC84v5qIioJuWTP%2F1vLFRaZK%2FATpu5vfM%3D"
+    });
   });
 });
--- tests/frontend-storage/src/tools/FrontendStorageIntegrationTests.ts
+++ tests/frontend-storage/src/tools/FrontendStorageIntegrationTests.ts
@@ -8,7 +8,7 @@ import * as path from "path";
 import * as cypress from "cypress";
 
 export class FrontendStorageIntegrationTests {
-  constructor(private readonly _supportFileSourcePath: string) {}
+  constructor(private readonly _supportFileSourcePath: string) { }
 
   public async start(): Promise<void> {
     const supportDir = path.resolve(__dirname, "..", "cypress", "support");
@@ -20,7 +20,7 @@ export class FrontendStorageIntegrationTests {
     fs.copyFileSync(this._supportFileSourcePath, supportFileTargetPath);
 
     const currentProjectRootAbsolutePath = path.resolve(__dirname, "..");
-    await cypress.run({
+    await cypress.open({
       browser: "chrome",
       project: currentProjectRootAbsolutePath,
       configFile: false,
