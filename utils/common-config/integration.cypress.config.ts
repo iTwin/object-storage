1e0af96a9c8b7c782370286294b7f3485e125799
Andrius Zonys <69573450+azonys@users.noreply.github.com>
2026-06-10 12:14:38 +0300

    Revert "Pass typescript module explicitly to getFullWebpackOptions"
    This reverts commit a2aacd9e3e08b02a5390607259fe7186d91632d1.

--- utils/common-config/integration.cypress.config.ts
+++ utils/common-config/integration.cypress.config.ts
@@ -4,12 +4,11 @@
  *--------------------------------------------------------------------------------------------*/
 import { defineConfig } from "cypress"
 import webpackPreprocessor from "@cypress/webpack-batteries-included-preprocessor";
-import * as typescript from "typescript";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function getWebpackOptions(): any {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const options: any = (webpackPreprocessor as any).getFullWebpackOptions(undefined, typescript);
+  const options: any = webpackPreprocessor.getFullWebpackOptions();
   const fallback = options.resolve.fallback;
   fallback.crypto = "crypto-browserify";
   fallback.events = "events";
